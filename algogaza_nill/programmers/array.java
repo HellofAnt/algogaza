d59ab475bd1868ba64a8bbff035c7bcbba05b798
nill <wwaosldk@gmail.com>
2020-07-17 23:42:59 +0900

    Refactoring!!

--- algogaza_nill/programmers/array.java
+++ algogaza_nill/programmers/array.java
@@ -4,71 +4,50 @@ import java.util.*;
 
 public class array {
     public static void main(String[] args) {
-        int[] answer;
-        int[] array = {1, 8, 2, 2, 10, 3, 7, 4};
-        int[][] commands = {{2, 5, 3},{4, 4, 1},{1, 7, 3},{1, 7, 3}};         
-        int[] test = new int[commands.length];
-        int index = 0;
-        System.out.println(test[2]);
-        System.out.println(array.length);
-        System.out.println(test);
+        //test case
+        int[] array = {1, 8, 2, 2, 10, 3, 244, 56, 2, 4, 2, 4, 2, 5};
+        int[][] commands = {{2, 12, 11},{4, 14, 10},{1, 7, 3},{1, 14, 2}};   
         
-        System.out.println("------------");
-
-
-        for(int q =0; q < commands.length; q++){
-            int[] array2 = new int[commands[q][1] - commands[q][0] + 1];
-            for(int j = 0; j <= commands[q][1] - commands[q][0]; j++){
-                array2[j] = array[commands[q][0] + j -1];
-            } 
-            System.out.println("자름");
-            System.out.println(Arrays.toString(array2));
-            // array = array2;
-            int[] r = new int[array2.length];
-        
-            for(int i = 0; i < array2.length; i++){
-                int k = 0;
-                for(int j = 0; j < array2.length; j++){
-                    if(array2[i] < array2[j]){
-                        k = k+1;
-                        r[i] = k;
-
-                    }
-                }
-            }
-            System.out.println("몇번째로 큰 수인지 랭크");
-            System.out.println(Arrays.toString(r));
-            int[] array3 = new int[array2.length];
-            for(int i = 0; i < array3.length; i++){
-                for(int j = i + 1; j < array3.length; j++){
-                    if(r[i] == r[j]){
-                        r[i] = r[i]+1;
-                        System.out.println("같은 수면 앞에꺼가 작은수로 랭크");
-                        System.out.println(Arrays.toString(r));
-                    }
-                }
-
-                array3[array2.length -r[i] - 1] = array2[i];
-                
-            }
-            System.out.println("랭크에 따라 재배열");
-            System.out.println(Arrays.toString(array3));
-
-            answer = array3;
-        System.out.println("---------------------");
-            
-        }        
-        
-        
-        System.out.println("------------");
-        System.out.println("------------");
-
-
-        
+        System.out.println(Arrays.toString(Solution.solution(array, commands)));
         
     }
+    static class Solution {
+        public static int[] solution(int[] array, int[][] commands) {
+            int[] answer = new int[commands.length];
+            // get answer factor
+            for(int q =0; q < commands.length; q++){
+                int[] cuted_array = new int[commands[q][1] - commands[q][0] + 1];
+                for(int j = 0; j <= commands[q][1] - commands[q][0]; j++){
+                    cuted_array[j] = array[commands[q][0] + j -1];
+                } 
+                int[]  arrayRank = new int[cuted_array.length];
+            
+                for(int i = 0; i < cuted_array.length; i++){
+                    int rank = 0;
+                    for(int j = 0; j < cuted_array.length; j++){
+                        if(cuted_array[i] < cuted_array[j]){
+                            rank = rank+1;
+                             arrayRank[i] = rank;
+    
+                        }
+                    }
+                }
+                int[] sorted_array = new int[cuted_array.length];
+                for(int i = 0; i < sorted_array.length; i++){
+                    // check and change the same rank
+                    for(int j = i + 1; j < sorted_array.length; j++){
+                        if( arrayRank[i] ==  arrayRank[j]){
+                             arrayRank[j] =  arrayRank[j]+1;
+                        }
+                    }
+                    sorted_array[cuted_array.length - arrayRank[i] - 1] = cuted_array[i];
+                }
+                answer[q] = sorted_array[commands[q][2]-1];
+            }
+            return answer;
+        }
+    }
 }
-
 
 /* class Solution {
     public static int[] solution(int[] array, int[][] commands) {
