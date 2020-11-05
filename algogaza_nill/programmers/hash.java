08e28a1cfbec7671af739f73b188fc5ade97d791
BuildTools <unconfigured@null.spigotmc.org>
2020-11-05 22:50:48 +0900

    hash-1

--- algogaza_nill/programmers/hash.java
+++ algogaza_nill/programmers/hash.java
@@ -1,5 +1,7 @@
 package programmers;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class hash {
     public static void main(String[] args) {
@@ -13,6 +15,7 @@ public class hash {
 
 class HashSolution {
     public String solution(String[] participant, String[] completion) {
+        /* 효율성 테스트 실패 
         String answer = "";
         StringBuilder sb = new StringBuilder();
         for(int i =0; i < participant.length; i++){
@@ -22,10 +25,25 @@ class HashSolution {
         }
         String appendedParticipant = sb.toString();
         for(int i = 0; i < completion.length; i++){
-            appendedParticipant = appendedParticipant.replaceFirst("/" + completion[i],"");
+            appendedParticipant = appendedParticipant.replaceFirst("/" + completion[i] + "/","/");
 
         }
         answer = appendedParticipant.replace("/", "");
+        return answer; */
+
+        String answer = "";
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < participant.length; i++) {
+            map.put(participant[i],map.getOrDefault(participant[i], 0) + 1);
+        }
+        for (int j = 0; j < completion.length; j++) {
+            map.put(completion[j], map.get(completion[j]) - 1);
+        }
+        for (String key : map.keySet()) {
+            if(map.get(key) == 1){
+                answer = key;
+            }
+        }
         return answer;
     }
 }
